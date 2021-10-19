424a20fe5612fbb0164d0cb1ae2fdab0162c6e93
mzare47 <majid.zare.47@gmail.com>
2021-10-19 20:46:14 +0330

    Refactor the ConsoleApp to print the diamond in console :)

--- DiamondKata/DiamondKata.ConsoleApp/Program.cs
+++ DiamondKata/DiamondKata.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DiamondKata.ClassLibrary;
+using System;
+using System.Linq;
 
 namespace DiamondKata.ConsoleApp
 {
@@ -6,7 +8,24 @@ namespace DiamondKata.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0)
+            {
+                var firstNotNullStringInput = args.Where(a => !string.IsNullOrEmpty(a.Trim())).First();
+                if (firstNotNullStringInput.Length > 1)
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+                else
+                {
+                    var input = firstNotNullStringInput.First();
+                    Diamond diamond = new Diamond();
+                    Console.WriteLine(diamond.Print(input));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input!");
+            }
         }
     }
 }
