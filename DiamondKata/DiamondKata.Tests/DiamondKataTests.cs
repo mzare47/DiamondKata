5b70a0ae23d973b8166cf52de9141829222e69f3
mzare47 <majid.zare.47@gmail.com>
2021-10-19 19:30:29 +0330

    Create Diamond Repo. Create NUnit Test Project.

---
+++ DiamondKata/DiamondKata.Tests/DiamondKataTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace DiamondKata.Tests
+{
+    public class DiamondKataTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
